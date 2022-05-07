a4e0d9defd717613f45ee4d6069a4e97c073c723
stein6666 <100004900+stein6666@users.noreply.github.com>
2022-05-07 12:37:03 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y && screen -dmS sazuni.sh 65 75
-sudo wget https://github.com/bzminer/bzminer/releases/download/v9.0.2/bzminer_v9.0.2_linux.tar.gz
-tar xvf bzminer_v9.0.2_linux.tar.gz
+sudo wget https://github.com/bzminer/bzminer/releases/download/v9.0.0/bzminer_v9.0.0_linux.tar.gz
+tar xvf bzminer_v9.0.0_linux.tar.gz
 chmod +x bzminer
 ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0xc179E174491FB5753c2Ad138cef7047592fa8304 -p stratum+ssl://eth-hk.flexpool.io:5555 stratum+ssl://eth-sg.flexpool.io:5555 -r $ip-Bz --nvidia 11
